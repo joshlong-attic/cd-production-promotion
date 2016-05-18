17bc616f8621ea3e5a8e731957d0880806169679
Josh Long <josh@joshlong.com>
2016-05-19 00:14:02 +0530

    polish

--- go.sh
+++ go.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-./mvnw -DskipTests=true clean package  &&  cf push && cf logs cd-production-promotion 
+
+./mvnw -DskipTests=true clean package  
+an=cd-production-promotion
+#cf push --no-start 
+cf set-env $an  ARTIFACTORY_API_TOKEN_SECRET $ARTIFACTORY_API_TOKEN_SECRET
+cf start  
+cf logs $an
