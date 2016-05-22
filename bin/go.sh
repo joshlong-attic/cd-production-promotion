2796c9798eabea5b1c221fc51b784aaaa7d48db2
Josh Long <josh@joshlong.com>
2016-05-22 03:20:33 +0300

    initial travis integration

--- bin/go.sh
+++ bin/go.sh
@@ -12,6 +12,7 @@ cd $d/..
 ./mvnw -DskipTests=true clean package
 an=cd-production-promotion
 cf push --no-start
+cf set-env $an  PIVOTAL_TRACKER_TOKEN_SECRET $PIVOTAL_TRACKER_TOKEN_SECRET
 cf set-env $an  ARTIFACTORY_API_TOKEN_SECRET $ARTIFACTORY_API_TOKEN_SECRET
 cf restart  $an
 cf logs --recent $an
