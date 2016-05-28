8a396a83715d866d9edc73930d646da333eaf845
Josh Long <josh@joshlong.com>
2016-05-27 20:35:57 -0400

    added bintray bits

--- bin/go.sh
+++ bin/go.sh
@@ -14,5 +14,7 @@ an=cd-production-promotion
 cf push --no-start
 cf set-env $an  PIVOTAL_TRACKER_TOKEN_SECRET $PIVOTAL_TRACKER_TOKEN_SECRET
 cf set-env $an  ARTIFACTORY_API_TOKEN_SECRET $ARTIFACTORY_API_TOKEN_SECRET
+cf set-env $an  BINTRAY_USERNAME $BINTRAY_USERNAME
+cf set-env $an  BINTRAY_PASSWORD $BINTRAY_PASSWORD
 cf restart  $an
 cf logs --recent $an
